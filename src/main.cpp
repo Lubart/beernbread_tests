745c8680709abb8cb5c13fa002756e9462ac3b8a
Heorhii <g.nosov@gmail.com>
2023-04-25 20:00:31 +0300

    Test C++20 PI constant

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,10 @@
 #include <iterator>
 #include <algorithm>
 
+#include <numbers> // std::numbers
+#include <iomanip>
+
+
 using namespace std;
 using namespace boost::lambda;
 typedef istream_iterator<int> in;
@@ -14,8 +18,14 @@ int main()
 {
     cout << "Adding 2 and 9 together gives you: " << add(2, 9) << endl;
 
-    for_each(in(cin), in(), cout << (_1 * 3) << " " );
+    //for_each(in(cin), in(), cout << (_1 * 3) << " " );
 
+
+    std::cout << std::fixed << std::setprecision(20);
+    std::cout << "float       " << std::numbers::pi_v<float> << std::endl;
+    std::cout << "double      " << std::numbers::pi << std::endl;
+    std::cout << "long double " << std::numbers::pi_v<long double> << std::endl;
+    std::cout << "exact       " << "3.141592653589793238462643383279502884197169399375105820974944" << std::endl;
 
     return 0;	
 }
